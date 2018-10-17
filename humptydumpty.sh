d0d8080bac14bbc76adaf13eab2801ecda5d5e50
root <root@whoami.berea.edu>
2018-10-17 18:27:02 -0400

    Version 0.3: Fixed the bash

--- humptydumpty.sh
+++ humptydumpty.sh
@@ -60,7 +60,7 @@ service_detection()
 {
 echo "Do you have time for the full scan? (y/n)"
 echo ""
-echo -n "${grn}༼(∩ ͡°╭͜ʖ╮͡ ͡°)༽⊃${end}━${yel}☆ﾟ. * ･ ｡ﾟ   ${end}" ; read sd
+echo -n "${grn} > > >  ${end}    " ; read sd
 if [[ $sd == "Y" || $sd == "y" ]]; then
 	touch service_detection.txt
 	echo -n "nmap -vv -p1-65535 -sV -oN mapIPCopIntUDPServDet.txt -iL target.txt" > service_detection.txt
@@ -77,82 +77,90 @@ fi
 
 main()
 {
-	echo "Welcome to the Humpty Dumpty!"
-	echo "PICTURE HERE"
-	echo "Let's find the target!!!"
-	intro
-	echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-	echo "In order to perform the firewall testing in the right order, I recommend following the following pathway:"
-	echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+	echo "Would you like to ${grn}start${end}? (y/n)"
 	echo ""
-	echo "${grn}1)${end} ${blu}Perform ACK scan${end}"
-	echo ""
-	echo "${grn}2)${end} ${blu}Perform TCP connect scan${end}"
-	echo ""
-	echo "${grn}3)${end} ${blu}Perform SYN scan${end}"
-	echo ""
-	echo "${grn}4)${end} ${blu}Perform Version Detection and Probing${end}"
-	echo ""
-	echo "${grn}5)${end} ${blu}Firewall testing: UDP and TCP Connect Scans${end}"
-	echo ""
-	echo "${grn}6)${end} ${blu}Service Detection scan${end}"
-	echo ""
-	echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
-	echo ""
-	echo "Would you like to go via ${cyn}recommended path (R)${end} or would you like to do it by ${mag}yourself(Y)${end}?"
-	echo ""
-	echo "Pick ${cyn}R${end} or ${mag}Y${end}"
-	echo -n "${grn}>>>${end} "; read choice
-	if [[ $choice == "R" || $choice == "r" ]]; then
-		ack
-		tcp_connect
-		syn
-		version_detection
-		udp_tcp
-		service_detection
-	elif [[ $choice == "Y" || $choice == "y" ]]; then
-		echo "Here are some options for you:"
-	    echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-		echo "In order to perform the firewall testing in the right order, I recommend following the following pathway:"
-	    echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-		echo "1) Perform ${red}ACK${end} scan"
-		echo ""
-		echo "2) Perform ${red}TCP connect${end} scan"
-		echo ""
-		echo "3) Perform ${red}SYN${end} scan"
-		echo ""
-		echo "4) Perform ${red}Version Detection and Probing${end}"
-		echo ""
-		echo "5) Firewall testing: ${red}UDP and TCP Connect${end} Scans"
-		echo ""
-		echo "6) ${red}Service Detection${end} scan"	
-		echo ""
+	echo -n "${grn} > > > ${end}    "; read start
+	if [[ $start == "Y" || $start == "y" ]]; then
+		echo "Welcome to the Humpty Dumpty!"
+		cat art/humptydumpty.txt
+		echo "Let's find the target!!!"
+		intro
 		echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-		echo "Which one would you like to do?"
+		echo "In order to perform the firewall testing in the right order, I recommend following the following pathway:"
+		echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 		echo ""
-		echo -n "${grn} > > > ${end}   " ; read ch
-		if [[ $ch == "1" ]]; then
+		echo " ${blu}Perform ACK scan${end}"
+		echo ""
+		echo " ${blu}Perform TCP connect scan${end}"
+		echo ""
+		echo " ${blu}Perform SYN scan${end}"
+		echo ""
+		echo " ${blu}Perform Version Detection and Probing${end}"
+		echo ""
+		echo " ${blu}Firewall testing: UDP and TCP Connect Scans${end}"
+		echo ""
+		echo " ${blu}Service Detection scan${end}"
+		echo ""
+		echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+		echo ""
+		echo "Would you like to go via ${cyn}recommended path (R)${end} or would you like to do it by ${mag}yourself(Y)${end}?"
+		echo ""
+		echo "Pick ${cyn}R${end} or ${mag}Y${end}"
+		echo -n "${grn}>>>${end} "; read choice
+		if [[ $choice == "R" || $choice == "r" ]]; then
 			ack
-			main
-		elif [[ $ch == "2" ]]; then
 			tcp_connect
-			main
-		elif [[ $ch == "3" ]]; then
 			syn
-		elif [[ $ch == "4" ]]; then
-			version_detection
-		elif [[ $ch == "5" ]]; then
 			udp_tcp
-		elif [[ $ch == "6" ]]; then
 			service_detection
-		else
+			version_detection
+		elif [[ $choice == "Y" || $choice == "y" ]]; then
+			echo "Here are some options for you"
+		    echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+			echo "In order to perform the firewall testing in the right order, I recommend following the following pathway:"
+		    echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+			echo "1) Perform ${red}ACK${end} scan"
+			echo ""
+			echo "2) Perform ${red}TCP connect${end} scan"
+			echo ""
+			echo "3) Perform ${red}SYN${end} scan"
+			echo ""
+			echo "4) Perform ${red}Version Detection and Probing${end}"
+			echo ""
+			echo "5) Firewall testing: ${red}UDP and TCP Connect${end} Scans"
+			echo ""
+			echo "6) ${red}Service Detection${end} scan"	
+			echo ""
+			echo "  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+			echo "Which one would you like to do?"
+			echo ""
+			echo -n "${grn} > > > ${end}   " ; read ch
+			if [[ $ch == "1" ]]; then
+				ack
+				main
+			elif [[ $ch == "2" ]]; then
+				tcp_connect
+				main
+			elif [[ $ch == "3" ]]; then
+				syn
+			elif [[ $ch == "4" ]]; then
+				version_detection
+			elif [[ $ch == "5" ]]; then
+				udp_tcp
+			elif [[ $ch == "6" ]]; then
+				service_detection
+			else
+				echo "I am not sure what you want"
+				exit
+			fi
+		else 
 			echo "I am not sure what you want"
 			exit
 		fi
-	else 
-		echo "I am not sure what you want"
+	elif [[ $start == "N" || $start == "n" ]]; then
+		cowsay -f ghostbusters Bye, you will never find joy in firewall intrusion
+	else
 		exit
-	fi
-
+fi
 }
 main
